ce975aa7dcddec78fd9968528627969428e99099
Your Name <you@example.com>
2023-03-20 01:01:57 +1100

    made modifications to code based on pylint complaints

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,12 +1,14 @@
-# Determine Andromeda location in ra/dec degrees
+# Determine Andromeda location in ra/dec degrees 
 
-# from wikipedia
-ra = '00:42:44.3'
-dec = '41:16:09'
+from math import cos, pi          
+from random import uniform
+
+# assign RA and Dec a constant value (for now) from wikipedia
+ra= '00:42:44.3' 
+dec = '41:16:09'  
+
 
 # convert to decimal degrees
-from math import *
-
 d, m, s = dec.split(':')
 dec = int(d)+int(m)/60+float(s)/3600
 
@@ -14,19 +16,21 @@ h, m, s = ra.split(':')
 ra = 15*(int(h)+int(m)/60+float(s)/3600)
 ra = ra/cos(dec*pi/180)
 
-nsrc = 1_000_000
+NSRC = 1_000_000
+
 
 # make 1000 stars within 1 degree of Andromeda
-from random import *
 ras = []
 decs = []
-for i in range(nsrc):
+for i in range(NSRC):
     ras.append(ra + uniform(-1,1))
     decs.append(dec + uniform(-1,1))
 
 
 # now write these to a csv file for use by my other program
-f = open('catalog.csv','w')
+f = open('catalog.csv','w', encoding='utf8')
+# ^- Consider using 'with' for resource-allocating operations (consider-using-with)
+
 print("id,ra,dec", file=f)
-for i in range(nsrc):
+for i in range(NSRC):
     print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
