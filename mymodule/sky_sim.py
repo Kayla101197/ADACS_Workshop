3be1ce2e4587b18215c4f9a843f806414216c54e
Your Name <you@example.com>
2023-03-20 01:28:23 +1100

    fixed all complaints raised by pylint (missed trailing whitespaces in last commit)

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -2,14 +2,14 @@
 A script to simulate a population of stars around the Andromeda galaxy.
 """
 
-import math          
+import math
 import random
 
 
-# determine Andromeda location in ra/dec degrees. 
+# determine Andromeda location in ra/dec degrees
 # assign RA and Dec a constant value (for now) from wikipedia
-RA = '00:42:44.3' 
-DEC = '41:16:09'  
+RA = '00:42:44.3'
+DEC = '41:16:09'
 
 # convert to decimal degrees
 d, m, s = DEC.split(':')
@@ -32,9 +32,6 @@ for i in range(NSRC):
 
 # now write these to a csv file for use by my other program
 with open('catalog.csv','w',encoding='utf-8') as f:
-
     print("id,ra,dec", file=f)
-
     for i in range(NSRC):
         print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
-#
